9b92f454ff67d2263d88ab5e3f7cb0b9cab37ffa
TicTac7x <tictac7x@gmail.com>
2021-11-12 18:46:41 +0200

    Start rewriting next and stop obstacle logic. Use lists to know exact order of obstacles. Marks of grace can be hardcoded.

--- src/main/java/tictac7x/rooftops/Obstacles.java
+++ src/main/java/tictac7x/rooftops/Obstacles.java
@@ -1,13 +1,73 @@
 package tictac7x.rooftops;
 
-import java.util.Set;
+import java.util.*;
+
 import net.runelite.api.Player;
+import net.runelite.api.Tile;
 import net.runelite.api.TileObject;
 import com.google.common.collect.ImmutableSet;
 
 public class Obstacles {
-    private static final Set<Integer> DRAYNOR_OBSTACLES = ImmutableSet.of(
-        11404,
+    public enum Region {
+        DRAYNOR,
+        ALKHARID,
+        VARROCK,
+        CANIFIS,
+        FALADOR,
+        SEERS,
+        POLLNIVNEACH
+    }
+
+    public static Region getRegion(final int[] regions) {
+        for (final int region : regions) {
+            if (DRAYNOR_REGIONS.contains(region)) {
+                return Region.DRAYNOR;
+            } else if (ALKHARID_REGIONS.contains(region)) {
+                return Region.ALKHARID;
+            } else if (VARROCK_REGIONS.contains(region)) {
+                return Region.VARROCK;
+            } else if (CANIFIS_REGIONS.contains(region)) {
+                return Region.CANIFIS;
+            } else if (FALADOR_REGIONS.contains(region)) {
+                return Region.FALADOR;
+            } else if (SEERS_REGIONS.contains(region)) {
+                return Region.SEERS;
+            } else if (POLLNIVNEACH_REGIONS.contains(region)) {
+                return Region.POLLNIVNEACH;
+            }
+        }
+
+        return null;
+    }
+
+    private static final int POSE_IDLE = 813;
+    private static final int ANIMATION_IDLE = -1;
+
+    public static List<Integer> getObstacles(final Region region) {
+        switch (region) {
+            case DRAYNOR:
+                return DRAYNOR_OBSTACLES;
+            case ALKHARID:
+                return ALKHARID_OBSTACLES;
+            case VARROCK:
+                return VARROCK_OBSTACLES;
+            case CANIFIS:
+                return CANIFIS_OBSTACLES;
+            case FALADOR:
+                return FALADOR_OBSTACLES;
+            case SEERS:
+                return SEERS_OBSTACLES;
+            case POLLNIVNEACH:
+                return POLLNIVNEACH_OBSTACLES;
+            default:
+                return null;
+        }
+    }
+
+    private static final Set<Integer> DRAYNOR_REGIONS = ImmutableSet.of(
+        12082, 12083, 12337, 12338, 12339, 12593, 12594, 12595
+    );
+    private static final List<Integer> DRAYNOR_OBSTACLES = Arrays.asList(11404,
         11405,
         11406,
         11430,
@@ -43,7 +103,10 @@ public class Obstacles {
         //
     );
 
-    private static final Set<Integer> ALKHARID_OBSTACLES = ImmutableSet.of(
+    private static final Set<Integer> ALKHARID_REGIONS = ImmutableSet.of(
+        12848, 12849, 12850, 13104, 13105, 13106, 13361, 13362
+    );
+    private static final List<Integer> ALKHARID_OBSTACLES = List.of(
         11633,
         14398,
         14402,
@@ -84,7 +147,10 @@ public class Obstacles {
         //
     );
 
-    private static final Set<Integer> VARROCK_OBSTACLES = ImmutableSet.of(
+    private static final Set<Integer> VARROCK_REGIONS = ImmutableSet.of(
+        12596, 12597, 12598, 12852, 12583, 12584
+    );
+    private static final List<Integer> VARROCK_OBSTACLES = List.of(
         14412,
         14413,
         14414,
@@ -129,7 +195,10 @@ public class Obstacles {
         //
     );
 
-    private static final Set<Integer> CANIFIS_OBSTACLES = ImmutableSet.of(
+    private static final Set<Integer> CANIFIS_REGIONS = ImmutableSet.of(
+        13621, 13622, 13623, 13877, 13878, 13879, 14133, 14134, 14135, 14390, 14391
+    );
+    private static final List<Integer> CANIFIS_OBSTACLES = List.of(
         14843,
         14844,
         14845,
@@ -170,7 +239,10 @@ public class Obstacles {
         //
     );
 
-    private static final Set<Integer> FALADOR_OBSTACLES = ImmutableSet.of(
+    private static final Set<Integer> FALADOR_REGIONS = ImmutableSet.of(
+        11827, 11828, 11829, 12083, 12084, 12085, 12339, 12340
+    );
+    private static final List<Integer> FALADOR_OBSTACLES = List.of(
         14898,
         14899,
         14901,
@@ -231,13 +303,24 @@ public class Obstacles {
         //
     );
 
-    private static final Set<Integer> SEERS_OBSTACLES = ImmutableSet.of(
+    private static final Set<Integer> SEERS_REGIONS = ImmutableSet.of(
+        10549, 10550, 10551, 10805, 10806, 10807, 11061, 11062, 11063
+    );
+    private static final Map<String, Integer> SEERS_MARKS = new HashMap<String, Integer>(){{
+        //
+        put("2727_3493", 14928);
+        put("2706_3493", 12932); put("2709_3493", 14932);
+        put("2712_3481", 14929);
+        //
+        put("2698_3465", 14931);
+    }};
+    private static final List<Integer> SEERS_OBSTACLES = List.of(
         14927,
         14928,
+        14932,
         14929,
         14930,
-        14931,
-        14932
+        14931
     );
     private static final Set<Integer> SEERS_ANIMATIONS = ImmutableSet.of(
         737, 1118,
@@ -264,6 +347,58 @@ public class Obstacles {
         //
     );
 
+    private static final Set<Integer> POLLNIVNEACH_REGIONS = ImmutableSet.of(
+        13101, 13102, 13103, 13358, 13359, 13613, 13614, 13615
+    );
+    private static final Map<String, Integer> POLLNIVNEACH_MARKS = new HashMap<String, Integer>(){{
+        put("3359_2983", 14941);
+        put("3357_3002", 14945);
+    }};
+    private static final List<Integer> POLLNIVNEACH_OBSTACLES = List.of(
+        14935,
+        14936,
+        14937,
+        14938,
+        14939,
+        14940,
+        14941,
+        14944,
+        14945
+    );
+    private static final Set<Integer> POLLNIVNEACH_ANIMATIONS = ImmutableSet.of(
+        2583, 2588,
+        1995, 1603, 2588,
+        1995, 1118, 2588,
+        2585,
+        1122, 1124, 2588,
+        828,
+        742, 743,
+        1603,
+        741, 2586, 2588
+    );
+    private static final Set<Integer> POLLNIVNEACH_POSES = ImmutableSet.of(
+        //
+        //
+        //
+        //
+        //
+        //
+        745, 744, 745
+        //
+        //
+    );
+    private static final Set<Integer> POLLNIVNEACH_IDLES = ImmutableSet.of(
+        //
+        //
+        //
+        //
+        //
+        //
+        745
+        //
+        //
+    );
+
     public static boolean isObstacle(final TileObject tile) {
         final int obstacle = tile.getId();
         return (false
@@ -273,37 +408,82 @@ public class Obstacles {
             || CANIFIS_OBSTACLES.contains(obstacle)
             || FALADOR_OBSTACLES.contains(obstacle)
             || SEERS_OBSTACLES.contains(obstacle)
+            || POLLNIVNEACH_OBSTACLES.contains(obstacle)
         );
     }
 
-    public static boolean isDoingObstacle(final Player player) {
+    public static boolean isDoingObstacle(final Region region, final Player player, final TileObject obstacle) {
         if (player != null) {
             final int animation = player.getAnimation();
             final int pose = player.getPoseAnimation();
             final int idle = player.getIdlePoseAnimation();
 
-            return (false
-                || DRAYNOR_ANIMATIONS.contains(animation)
-                || DRAYNOR_POSES.contains(pose)
-                || DRAYNOR_IDLES.contains(idle)
-                || ALKHARID_ANIMATIONS.contains(animation)
-                || ALKHARID_POSES.contains(pose)
-                || ALKHARID_IDLES.contains(idle)
-                || VARROCK_ANIMATIONS.contains(animation)
-                || VARROCK_POSES.contains(pose)
-                || VARROCK_IDLES.contains(idle)
-                || CANIFIS_ANIMATIONS.contains(animation)
-                || CANIFIS_POSES.contains(pose)
-                || CANIFIS_IDLES.contains(idle)
-                || FALADOR_ANIMATIONS.contains(animation)
-                || FALADOR_POSES.contains(pose)
-                || FALADOR_IDLES.contains(idle)
-                || SEERS_ANIMATIONS.contains(animation)
-                || SEERS_POSES.contains(pose)
-                || SEERS_IDLES.contains(idle)
+            final Integer distance_world;
+            if (obstacle != null) {
+                distance_world = player.getWorldLocation().distanceTo(obstacle.getWorldLocation());
+            } else {
+                distance_world = null;
+            }
+
+            return (
+                obstacle != null && distance_world == Integer.MAX_VALUE && idle == POSE_IDLE && animation != ANIMATION_IDLE
+
+                || region == Region.DRAYNOR && (
+                    DRAYNOR_ANIMATIONS.contains(animation)
+                    || DRAYNOR_POSES.contains(pose)
+                    || DRAYNOR_IDLES.contains(idle)
+                )
+
+                || region == Region.ALKHARID && (
+                    ALKHARID_ANIMATIONS.contains(animation)
+                    || ALKHARID_POSES.contains(pose)
+                    || ALKHARID_IDLES.contains(idle)
+                )
+
+                || region == Region.VARROCK && (
+                    VARROCK_ANIMATIONS.contains(animation)
+                    || VARROCK_POSES.contains(pose)
+                    || VARROCK_IDLES.contains(idle)
+                )
+
+                || region == Region.CANIFIS && (
+                    CANIFIS_ANIMATIONS.contains(animation)
+                    || CANIFIS_POSES.contains(pose)
+                    || CANIFIS_IDLES.contains(idle)
+                )
+
+                || region == Region.FALADOR && (
+                    FALADOR_ANIMATIONS.contains(animation)
+                    || FALADOR_POSES.contains(pose)
+                    || FALADOR_IDLES.contains(idle)
+                )
+
+                || region == Region.SEERS && (
+                    SEERS_ANIMATIONS.contains(animation)
+                    || SEERS_POSES.contains(pose)
+                    || SEERS_IDLES.contains(idle)
+                )
+
+                || region == Region.POLLNIVNEACH && (
+                    POLLNIVNEACH_ANIMATIONS.contains(animation)
+                    || POLLNIVNEACH_POSES.contains(pose)
+                    || POLLNIVNEACH_IDLES.contains(idle)
+                )
             );
         }
 
         return false;
+    }
+
+    public static Optional<Integer> getMarkObstacle(final Region region, final Tile tile) {
+        final String location = tile.getWorldLocation().getX() + "_" + tile.getWorldLocation().getY();
+
+        if (region == Region.SEERS && SEERS_MARKS.containsKey(location)) {
+            return Optional.of(SEERS_MARKS.get(location));
+        } else if (region == Region.POLLNIVNEACH && POLLNIVNEACH_MARKS.containsKey(location)) {
+            return Optional.of(POLLNIVNEACH_MARKS.get(location));
+        }
+
+        return Optional.empty();
     }
 }
--- src/main/java/tictac7x/rooftops/RooftopsPlugin.java
+++ src/main/java/tictac7x/rooftops/RooftopsPlugin.java
@@ -5,21 +5,12 @@ import net.runelite.api.Client;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.GameState;
 import com.google.inject.Provides;
+import net.runelite.api.events.*;
 import net.runelite.client.plugins.Plugin;
-import net.runelite.api.events.ItemSpawned;
-import net.runelite.api.events.ItemDespawned;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.MenuOptionClicked;
-import net.runelite.api.events.GameObjectSpawned;
-import net.runelite.api.events.GameObjectDespawned;
-import net.runelite.api.events.GroundObjectSpawned;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.api.events.GroundObjectDespawned;
 import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.api.events.DecorativeObjectSpawned;
-import net.runelite.api.events.DecorativeObjectDespawned;
 
 @Slf4j
 @PluginDescriptor(
@@ -37,8 +28,9 @@ public class RooftopsPlugin extends Plugin {
 	@Inject
 	private RooftopsConfig config;
 
-	private RooftopsOverlayObstacles overlay_obstacles;
-	private RooftopsOverlayMarks overlay_marks;
+	private RooftopsOverlay overlay;
+
+	private OverylayDebug debug;
 
 	@Provides
 	RooftopsConfig provideConfig(ConfigManager configManager) {
@@ -47,70 +39,75 @@ public class RooftopsPlugin extends Plugin {
 
 	@Override
 	protected void startUp() {
-		if (overlay_obstacles == null) {
-			overlay_marks = new RooftopsOverlayMarks(config);
-			overlay_obstacles = new RooftopsOverlayObstacles(config, client, overlay_marks);
+		if (overlay == null) {
+			overlay = new RooftopsOverlay(config, client);
+			debug = new OverylayDebug();
 		}
 
-		overlays.add(overlay_obstacles);
-		overlays.add(overlay_marks);
+		overlays.add(overlay);
+		overlays.add(debug);
 	}
 
 	@Override
 	protected void shutDown() {
-		overlays.remove(overlay_obstacles);
-		overlays.remove(overlay_marks);
+		overlays.remove(overlay);
+		overlays.remove(debug);
 	}
 
 	@Subscribe
 	public void onGameObjectSpawned(final GameObjectSpawned event) {
-		overlay_obstacles.onTileObjectSpawned(event.getGameObject());
+		overlay.onTileObjectSpawned(event.getGameObject());
 	}
 
 	@Subscribe
 	public void onGameObjectDespawned(final GameObjectDespawned event) {
-		overlay_obstacles.onTileObjectDespawned(event.getGameObject());
+		overlay.onTileObjectDespawned(event.getGameObject());
 	}
 
 	@Subscribe
 	public void onGroundObjectSpawned(final GroundObjectSpawned event) {
-		overlay_obstacles.onTileObjectSpawned(event.getGroundObject());
+		overlay.onTileObjectSpawned(event.getGroundObject());
 	}
 
 	@Subscribe
 	public void onGroundObjectDespawned(final GroundObjectDespawned event) {
-		overlay_obstacles.onTileObjectDespawned(event.getGroundObject());
+		overlay.onTileObjectDespawned(event.getGroundObject());
 	}
 
 	@Subscribe
 	public void onDecorativeObjectSpawned(final DecorativeObjectSpawned event) {
-		overlay_obstacles.onTileObjectSpawned(event.getDecorativeObject());
+		overlay.onTileObjectSpawned(event.getDecorativeObject());
 	}
 
 	@Subscribe
 	public void onDecorativeObjectDespawned(final DecorativeObjectDespawned event) {
-		overlay_obstacles.onTileObjectDespawned(event.getDecorativeObject());
+		overlay.onTileObjectDespawned(event.getDecorativeObject());
 	}
 
 	@Subscribe
 	public void onItemSpawned(final ItemSpawned event) {
-		overlay_marks.onItemSpawned(event);
+		overlay.onItemSpawned(event);
 	}
 
 	@Subscribe
 	public void onItemDespawned(final ItemDespawned event) {
-		overlay_marks.onItemDespawned(event);
+		overlay.onItemDespawned(event);
 	}
 
 	@Subscribe
 	public void onMenuOptionClicked(final MenuOptionClicked menu_option_clicked) {
-		overlay_obstacles.onMenuOptionClicked(menu_option_clicked);
+		overlay.onMenuOptionClicked(menu_option_clicked);
+	}
+
+	@Subscribe
+	public void onStatChanged(final StatChanged stat_changed) {
+		overlay.onStatChanged(stat_changed);
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged event) {
 		if (event.getGameState() == GameState.LOADING) {
-			overlay_obstacles.clear();
+			overlay.clear();
 		}
 	}
 }
