fa1c8ac9d0b5f203d5110a2d6e9e70013c1aa500
TicTac7x <tictac7x@gmail.com>
2021-12-01 01:11:40 +0200

    Config order.

--- src/main/java/tictac7x/storage/StorageConfig.java
+++ src/main/java/tictac7x/storage/StorageConfig.java
@@ -10,40 +10,9 @@ public interface StorageConfig extends Config {
 	String group = "tictac7x-storage";
 
 	@ConfigSection(
-		name = "Bank",
-		description = "Bank overlay",
-		position = 1
-	) String bank = "bank";
-
-		@ConfigItem(
-			keyName = "bank",
-			name = "bank",
-			description = "bank",
-			section = bank,
-			position = 1,
-			hidden = false
-		) default String getBank() { return ""; }
-
-		@ConfigItem(
-			keyName = "bank_whitelist",
-			name = "whitelist",
-			description = "Names of items to show in the bank overlay.",
-			section = bank,
-			position = 2
-		) default String getBankWhitelist() { return ""; }
-
-		@ConfigItem(
-			keyName = "bank_blacklist",
-			name = "blacklist",
-			description = "Names of items to hide from the bank overlay.",
-			section = bank,
-			position = 3
-		) default String getBankBlacklist() { return ""; }
-
-	@ConfigSection(
 		name = "Inventory",
 		description = "Inventory overlay",
-		position = 2
+		position = 1
 	) String inventory = "inventory";
 
 		@ConfigItem(
@@ -70,4 +39,35 @@ public interface StorageConfig extends Config {
 			section = inventory,
 			position = 3
 		) default String getInventoryBlacklist() { return ""; }
+
+	@ConfigSection(
+		name = "Bank",
+		description = "Bank overlay",
+		position = 2
+	) String bank = "bank";
+
+		@ConfigItem(
+			keyName = "bank",
+			name = "bank",
+			description = "bank",
+			section = bank,
+			position = 1,
+			hidden = false
+		) default String getBank() { return ""; }
+
+		@ConfigItem(
+			keyName = "bank_whitelist",
+			name = "whitelist",
+			description = "Names of items to show in the bank overlay.",
+			section = bank,
+			position = 2
+		) default String getBankWhitelist() { return ""; }
+
+		@ConfigItem(
+			keyName = "bank_blacklist",
+			name = "blacklist",
+			description = "Names of items to hide from the bank overlay.",
+			section = bank,
+			position = 3
+		) default String getBankBlacklist() { return ""; }
 }
