41f893855e978bb7304e8a434313b4a72a226334
aekramer <aekramer@hotmail.com>
2021-12-21 15:08:58 +0100

    Added support for Ardougne course

---
+++ src/main/java/tictac7x/rooftops/courses/RooftopCourseArdougne.java
@@ -0,0 +1,58 @@
+package tictac7x.rooftops.courses;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Arrays;
+
+public class RooftopCourseArdougne extends Course {
+    public RooftopCourseArdougne() {
+        super(
+            // Obstacles.
+            Arrays.asList(
+                15608,
+                15609,
+                26635,
+                15610,
+                15611,
+                28912,
+                15612
+            ),
+
+            // Animations.
+            ImmutableSet.of(
+                737, 2588,
+                2586, 2588, 2586, 2588, 2583, 2588,
+                //
+                2586, 2588,
+                7133, 2588,
+                753, 759,
+                2586, 2588, 741, 741, 2586, 2588
+            ),
+
+            // Poses.
+            ImmutableSet.of(
+                //821, 819,
+                //819, 823,
+                762, //819,
+                //823,
+                //823,
+                757, 756, 757 // 819, 823,
+                //823, 824, 819
+            ),
+
+            // Idles.
+            ImmutableSet.of(
+                //
+                //
+                765,
+                //
+                //
+                757
+                //
+                //
+            ),
+
+            ImmutableSet.of()
+        );
+    }
+}
