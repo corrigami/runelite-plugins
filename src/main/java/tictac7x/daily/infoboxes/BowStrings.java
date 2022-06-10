87fa9676f159177fd06d027506b366504b5d80b7
TicTac7x <tictac7x@gmail.com>
2022-06-10 22:10:18 +0300

    Fix bow strings id, improve renderSupplier, simplify getFlaxAmount

--- src/main/java/tictac7x/daily/infoboxes/BowStrings.java
+++ src/main/java/tictac7x/daily/infoboxes/BowStrings.java
@@ -14,41 +14,37 @@ public class BowStrings extends DailyInfobox {
     private final String tooltip = "Exchange flax to %d bow strings from the Flax Keeper at Seers Village";
 
     public BowStrings(final Client client, final DailyConfig config, final ItemManager items, final TicTac7xDailyPlugin plugin) {
-        super(client, config, DailyConfig.pure_essence_id, items.getImage(ItemID.BOW_STRING), plugin);
+        super(client, config, DailyConfig.bow_strings_id, items.getImage(ItemID.BOW_STRING), plugin);
     }
 
     @Override
     public Supplier<Boolean> getRenderSupplier() {
         return () -> (
-                config.showBowStrings() &&
-                        client.getVarbitValue(Varbits.DIARY_KANDARIN_EASY) == 1 &&
-                        client.getVarbitValue(Varbits.DAILY_FLAX_STATE) == 0 &&
-                        getFlaxAmount() > 0
+            config.showBowStrings() &&
+            client.getVarbitValue(Varbits.DIARY_KANDARIN_EASY) == 1 &&
+            client.getVarbitValue(Varbits.DAILY_FLAX_STATE) == 0
         );
     }
 
     @Override
     public Supplier<String> getTextSupplier() {
-        return () -> String.valueOf(getFlaxAmount());
+        return () -> String.valueOf(getBowStringsAmount());
     }
 
     @Override
     public Supplier<String> getTooltipSupplier() {
-        return () -> String.format(tooltip, getFlaxAmount());
+        return () -> String.format(tooltip, getBowStringsAmount());
     }
 
-    private int getFlaxAmount() {
-        int flax;
-
+    private int getBowStringsAmount() {
         if (client.getVarbitValue(Varbits.DIARY_KANDARIN_EASY) == 1) {
             if (client.getVarbitValue(Varbits.DIARY_KANDARIN_MEDIUM) == 1) {
                 if (client.getVarbitValue(Varbits.DIARY_KANDARIN_HARD) == 1) {
                     if (client.getVarbitValue(Varbits.DIARY_KANDARIN_ELITE) == 1) {
-                        flax = 250;
-                    } else flax = 120;
-                } else flax = 60;
-            } else flax = 30;
-        } else flax = 0;
-        return flax;
+                        return 250;
+                    } return 120;
+                } return 60;
+            } return 30;
+        } return 0;
     }
 }
